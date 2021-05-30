c35f29f881f05bb016da4795511158b923d0d387
dunjazamaklar@uns.ac.rs <dunjazamaklar@uns.ac.rs>
2021-05-30 13:05:15 +0200

    vezbe2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatusComponent } from './components/status/status.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatExpansionModule } from '@angular/material/expansion';
+import { AboutComponent } from './components/core/about/about.component';
+import { HomeComponent } from './components/core/home/home.component';
+import { AuthorComponent } from './components/core/author/author.component';
+import { FakultetComponent } from './components/fakultet/fakultet.component';
+import { StudentComponent } from './components/student/student.component';
+import { DepartmanComponent } from './components/departman/departman.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusComponent,
+    AboutComponent,
+    HomeComponent,
+    AuthorComponent,
+    FakultetComponent,
+    StudentComponent,
+    DepartmanComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
