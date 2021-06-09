779c7ecf927b7effe732e5e41be4729242b4490b
DunjaZamaklar <56863991+DunjaZamaklar@users.noreply.github.com>
2021-06-09 11:56:58 +0200

    finalChangesForTest2

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,13 @@
+/*
 export const Status_URL='http://localhost:8083/Status';
 export const Student_URL='http://localhost:8083/Student';
 export const Fakultet_URL='http://localhost:8083/Fakultet';
 export const Departman_URL='http://localhost:8083/Departman';
 export const Student_URL_Departman='http://localhost:8083/StudentiDepartmanId';
+*/
+
+export const Status_URL='https://backend-it19-2018.herokuapp.com/Status';
+export const Student_URL='https://backend-it19-2018.herokuapp.com/Student';
+export const Fakultet_URL='https://backend-it19-2018.herokuapp.com/Fakultet';
+export const Departman_URL='https://backend-it19-2018.herokuapp.com/Departman';
+export const Student_URL_Departman='https://backend-it19-2018.herokuapp.com/StudentiDepartmanId';
